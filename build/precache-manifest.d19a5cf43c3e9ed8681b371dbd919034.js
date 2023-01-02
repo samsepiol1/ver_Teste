9de10f7d78d754402285e1ab17befdefce3054a3
samsepiol1 <steve.jkl5@gmail.com>
2023-01-02 17:01:56 -0300

    adicionar mensagens

---
+++ build/precache-manifest.d19a5cf43c3e9ed8681b371dbd919034.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee10d12d5e9171581e8fbc815f11a14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2e880eae6384297f480",
+    "url": "/static/css/main.61c26774.chunk.css"
+  },
+  {
+    "revision": "70900cddbbf777291f51",
+    "url": "/static/js/2.6c98417f.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.6c98417f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e880eae6384297f480",
+    "url": "/static/js/main.0b7dcbb3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/static/media/8secondz.9388d614.png"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "9ef3cb7262237d548954ce357584c36f",
+    "url": "/static/media/cactuscan.9ef3cb72.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/javascript.0cded3a3.png"
+  },
+  {
+    "revision": "0a88efbeace9a1d6aa5197213b1bce21",
+    "url": "/static/media/laniaq.0a88efbe.png"
+  },
+  {
+    "revision": "b064c21037b85dcf274c8d11413e0c7a",
+    "url": "/static/media/lucas.b064c210.png"
+  },
+  {
+    "revision": "e2f97163eb561e4a78c17db671d601f0",
+    "url": "/static/media/outgo.e2f97163.png"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/static/media/psykinetic.9c1952ca.png"
+  },
+  {
+    "revision": "19e43c1141b4f35fc9aae15f8df1ecc6",
+    "url": "/static/media/python.19e43c11.webp"
+  },
+  {
+    "revision": "cb75cc0083b5ccedbb4541df0ea89439",
+    "url": "/static/media/ruby.cb75cc00.png"
+  },
+  {
+    "revision": "acece89dc125a9da880138538fc21d82",
+    "url": "/static/media/sql.acece89d.png"
+  },
+  {
+    "revision": "6f71da66ec5832ac90245340e7ef63b9",
+    "url": "/static/media/ufrn.6f71da66.png"
+  }
+]);
